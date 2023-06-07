fa97e213272f1c4dfcb3eedf4fcc9d871f22548b
Rue-ru <st10186913@imconnect.edu.za>
2023-06-07 14:33:42 +0200

    Adding child class Recipe

---
+++ POE_PART2/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART2
+{
+    class Ingredient
+    {
+        public string IngredientName { get; set; }
+        public double Quantity { get; set; }
+        public string RecipeUnitofMeasure { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient(string ingredientName, double quantity, string recipeUnitofMeasure, int calories, string foodGroup)
+        {
+            IngredientName = ingredientName;
+            Quantity = quantity;
+            RecipeUnitofMeasure = recipeUnitofMeasure;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+    }
+}
---
+++ POE_PART2/Recipe.cs
@@ -0,0 +1,38 @@
+﻿using POE_PART2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART2
+{
+    public class Recipe
+    {
+
+        public string RecipeName { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+        public List<string> Description { get; set; }
+        public int Quantity { get; set; }
+        public int RecipeUnitsofMeasure { get; set; }
+        public int RecipeSteps { get; set; }
+
+
+        public Recipe(string recipeName)
+        {
+            RecipeName = recipeName;
+            Ingredients = new List<Ingredient>();
+            Description = new List<string>();
+        }
+        public void AddingIngredient(Ingredient ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        public void AddingStep(string recipeSteps)
+        {
+            RecipeSteps.Add(recipeSteps);
+        }
+
+    }
+}
